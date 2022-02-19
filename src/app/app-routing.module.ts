c4d0a552138fd8828ac83ac6d3e17292da31a155
Karthik-Vijay <karthik.vijay@rabobank.nl>
2022-02-19 21:51:30 +0530

    loosely coupled components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { GameTwoComponent } from './games/game-two/game-two.component';
 import { LoginComponent } from './login/login.component';
 import { TyperComponent } from './common-components/typer/typer.component';
 import { GameOneComponent } from './games/game-one/game-one.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  
+  {path: '', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
   { path: 'app-typer', component: TyperComponent },
   { path: 'game-two', component: GameTwoComponent },
   { path: 'app-login', component: LoginComponent },
-  // temp
   { path: 'game-one', component: GameOneComponent }
 ];
 
