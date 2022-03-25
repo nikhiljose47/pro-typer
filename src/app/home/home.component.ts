5c7bb485d0e7100ee157b8628344d925d38e1bd9
Karthik-Vijay <karthik.vijay@rabobank.nl>
2022-03-25 18:21:06 +0530

    fix replay

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -126,7 +126,6 @@ export class HomeComponent implements OnInit {
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
-      this.changeTyper();
     });
   }
 
