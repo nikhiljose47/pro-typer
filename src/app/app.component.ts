c5d6178d83092045325c13082b110e28320d4e76
Karthik-Vijay <karthikvijaychry@gmail.com>
2022-06-08 13:04:13 +0530

    (styling): multiplayer game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,7 +6,7 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'Typer pro';
 
   constructor( private titleService: Title,
@@ -35,5 +35,9 @@ export class AppComponent implements OnInit {
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
+  }
+
+  ngOnDestroy() {
+    localStorage.clear();
   }
 }
