5b71ee9f5cdc0c8aa4a6bc71b6a95a9534ce529d
Karthik-Vijay <karthikvijaychry@gmail.com>
2022-06-15 13:20:06 +0530

    removed ng circle progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TyperComponent } from './components/common-components/typer/typer.component';
@@ -15,7 +14,7 @@ import { TyperReplayComponent } from './components/typer-replay/typer-replay.com
 import { TyperPracticeComponent } from './components/typer-practice/typer-practice.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule} from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -27,7 +26,6 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
 import { GameThreeComponent } from './components/games/game-three/game-three.component';
 import { GameThreeUnitComponent } from './components/games/game-three-unit/game-three-unit.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FooterComponent } from './components/footer/footer.component';
 import { GameFourComponent } from './components/games/game-four/game-four.component';
 import { DialogBoxComponent } from './components/common-components/dialog-box/dialog-box.component';
@@ -65,22 +63,7 @@ import { DialogBoxComponent } from './components/common-components/dialog-box/di
     BrowserAnimationsModule,
     MatDialogModule,
     MatProgressSpinnerModule,
-    ScrollingModule,
-    NgCircleProgressModule.forRoot({
-      "subtitle": "sec",
-      "units": "seconds",
-      "responsive": true,
-      "lazy": false,
-      "titleFontSize": '32px',
-      "titleFontWeight": '600',
-      "subtitleFontSize": '20px',
-      "backgroundGradient": true,
-      "backgroundColor": "white",
-      "backgroundPadding": 0.5,
-      "backgroundGradientStopColor": "rgb(238, 238, 238)",
-      "outerStrokeGradient": false,
-      "showInnerStroke": false,
-    })
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
