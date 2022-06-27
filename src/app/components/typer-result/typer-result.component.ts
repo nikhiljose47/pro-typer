16d946ea90bfbf6d3bd56fad68ed0c0bed19bf5b
nikhiljose47 <nikhiljose47@gmail.com>
2022-06-27 20:41:52 +0530

    updated typer result with details & emojis0.9

--- src/app/components/typer-result/typer-result.component.ts
+++ src/app/components/typer-result/typer-result.component.ts
@@ -8,9 +8,9 @@ import { HomeComponent } from '../home/home.component';
   styleUrls: ['./typer-result.component.scss']
 })
 export class TyperResultComponent implements OnInit {
-  isLoading: boolean = true;
   showMarks: boolean = false;
-
+  imageName: string;
+  description: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<HomeComponent>) { }
@@ -18,7 +18,27 @@ export class TyperResultComponent implements OnInit {
   ngOnInit(): void {
     if (this.data.wpm) {
       this.showMarks = true;
-      setTimeout(() => this.isLoading = false, 2000);
+      this.createResultDesc();
+    }
+  }
+
+  createResultDesc():void{
+    let wpm = parseInt(this.data.wpm);
+    if(wpm >60){
+      this.imageName = "result_super";
+      this.description = "Ultra!, you are highly productive!";
+    }
+    else if(wpm >50){
+      this.imageName = "result_good";
+      this.description = "Nice!, you are fast!";
+    }
+    else if(wpm>38){
+      this.imageName = "result_good";
+      this.description = "Yes!, You are above world average typers!";
+    }
+    else{
+      this.imageName = "result_sad";
+      this.description = "Too slow!, you need more practice!";
     }
   }
 
